243aa245b3bc6ee283fc78cae8bc41280649ada1
qb1968 <qb1968@aol.com>
2020-06-19 11:35:02 -0400

    Sprint

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -3,26 +3,32 @@
 
 import sys
 
-LDI = 0b10000010
-PRN = 0b01000111
-HLT = 0b00000001
-MUL = 0b10100010
-PUSH = 0b01000101
-POP = 0b01000110
-CALL = 0b01010000
-RET = 0b00010001
-ADD = 0b10100000
 
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        
         self.pc = 0
         self.reg = [0] * 8
         self.ram = [0] * 256
-        self.SP = 0xf3
+        self.SP = 7
+        self.reg[self.SP] = 0xF4
+        self.flag = {}
+        self.LDI = 0b10000010
+        self.PRN = 0b01000111
+        self.HLT = 0b00000001
+        self.MUL = 0b10100010
+        self.PUSH = 0b01000101
+        self.POP = 0b01000110
+        self.CALL = 0b01010000
+        self.RET = 0b00010001
+        self.ADD = 0b10100000
+        self.CMP = 0b10100111
+        self.JMP = 0b01010100
+        self.JEQ = 0b01010101
+        self.JNE = 0b01010110
         
 
     def load(self, filename):
@@ -64,12 +70,26 @@ class CPU:
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
-        if op == "ADD":
+        if op == self.ADD:
             self.reg[reg_a] += self.reg[reg_b]
-        elif op == MUL:
+        elif op == self.MUL:
             self.reg[reg_a] *= self.reg[reg_b]
+        elif op == self.CMP:
+            if self.reg[reg_a] == self.reg[reg_b]:
+                self.flag['E'] = 1
+            else:
+                self.flag['E'] = 0
+            if self.reg[reg_a] < self.reg[reg_b]:
+                self.flag['L'] = 1
+            else:
+                self.flag['L'] = 0
+            if self.reg[reg_a] > self.reg[reg_b]:
+                self.flag['G'] = 1
+            else:
+                self.flag['G'] = 0
+        
         else:
-            raise Exception("Unsupported ALU operation")
+            raise Exception("Unsupported ALU operation")    
 
     def trace(self):
         """
@@ -92,45 +112,66 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        while True:
-            ops = self.ram[self.pc]
+        halted = False
+        while not halted:
+            # IR = Instruction Register, contains a copy of the currently executing instruction
+            IR = self.ram[self.pc]
             operand_a = self.ram_read(self.pc + 1)
             operand_b = self.ram_read(self.pc + 2)
             # print(IR, self.pc, operand_a, operand_b)
-            if ops == LDI:
+            if IR == self.LDI:
                 self.reg[operand_a] = operand_b
                 # print(self.pc, self.reg, self.ram)
                 self.pc += 3
-            elif ops == PRN:
+            elif IR == self.PRN:
                 print(self.reg[operand_a])
                 self.pc += 2
-            elif ops == MUL:
-                self.alu(ops, operand_a, operand_b)
+            elif IR == self.MUL:
+                self.alu(IR, operand_a, operand_b)
                 self.pc += 3
-            elif ops == PUSH:
+            elif IR == self.PUSH:
                 operand_a = self.ram_read(self.pc + 1) 
                 self.ram_write(self.SP, self.reg[operand_a])
                 self.pc += 2
                 self.SP -= 1
-            elif ops == POP:
+            elif IR == self.POP:
                 operand_a = self.ram_read(self.pc + 1)
                 self.reg[operand_a] = self.ram_read(self.SP+1)
                 self.SP += 1
                 self.pc += 2
-            elif ops == CALL:
-                    # print(operand_a)
+            elif IR == self.CALL:
+                # print(operand_a)
                 operand_a = self.ram_read(self.pc + 1)
                 self.SP -= 1
                 self.ram_write(self.SP, self.pc + 2)
                 self.pc = self.reg[operand_a]
                 # print(self.pc)
-            elif ops == RET:
+            elif IR == self.RET:
                 self.pc = self.ram_read(self.SP)
                 self.SP += 1
-            elif ops == ADD:
+            elif IR == self.ADD:
                 self.pc +=3
-                self.alu("ADD", operand_a, operand_b)        
-            elif ops == HLT:
+                self.alu(self.ADD, operand_a, operand_b)
+            elif IR == self.CMP:
+                self.alu(self.CMP, operand_a, operand_b)
+                self.pc += 3
+            # Jump to the address stored in the given register.
+            # Set the PC to the address stored in the given register.
+            elif IR == self.JMP:
+                self.pc = self.reg[operand_a]
+            # If equal flag is set (true), jump to the address stored in the given register.
+            elif IR == self.JEQ:
+                if self.flag['E'] == 1:
+                    self.pc = self.reg[operand_a]
+                else:
+                    self.pc += 2
+            # If E flag is clear (false, 0), jump to the address stored in the given register.
+            elif IR == self.JNE:
+                if self.flag['E'] == 0:
+                    self.pc = self.reg[operand_a]
+                else:
+                    self.pc += 2
+            elif IR == self.HLT:
                 sys.exit(0)
             else:
                 print(f"Did not work")
